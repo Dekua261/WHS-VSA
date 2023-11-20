df53511bfdf2c3db5cb75e7f752b3ac27e540456
andrewtran777 <andrewtran559@gmail.com>
2023-11-19 22:21:57 -0800

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/">
       <Routes>
-        <Route path="/WHS-VSA">
+        <Route path="/">
           <Route index element={<Home />} />
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
+      <Outlet />
     </Router>
   );
 }
