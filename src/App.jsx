6266b77be0b32f72dce338d6ec2a6ed7d6362265
andrewtran777 <andrewtran559@gmail.com>
2023-11-19 22:26:29 -0800

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HashRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import About from "./pages/About";
 import NoMatch from "./pages/NoMatch";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route path="/">
           <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
